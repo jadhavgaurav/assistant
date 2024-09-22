aefd81d074feb160ccfb38398010e827ac2c2f7d
Gaurav Vijay Jadhav <gauravvjadhav1432@gmail.com>
2024-09-22 23:40:45 +0530

    whatsapp features added

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 con = sqlite3.connect("victus.db")
@@ -17,30 +18,74 @@ cursor.execute(query)
 query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
 
-query = "INSERT INTO web_command VALUES (null,'canva', 'https://www.canva.com/en_in/')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'canva', 'https://www.canva.com/en_in/')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'whatsapp', 'https://web.whatsapp.com')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'whatsapp', 'https://web.whatsapp.com')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'chatgpt', 'https://chatgpt.com/')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'chatgpt', 'https://chatgpt.com/')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'youtube', 'https://youtube.com')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://youtube.com')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'wingz', 'https://wingz.itvedant.com/index.php/site/login')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'wingz', 'https://wingz.itvedant.com/index.php/site/login')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'wings', 'https://wingz.itvedant.com/index.php/site/login')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'wings', 'https://wingz.itvedant.com/index.php/site/login')"
+# cursor.execute(query)
+# con.commit()
 
-query = "INSERT INTO web_command VALUES (null,'Hackerrank', 'https://www.hackerrank.com/dashboard')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'Hackerrank', 'https://www.hackerrank.com/dashboard')"
+# cursor.execute(query)
+# con.commit()
+
+## Create contacts Table
+## Create a table with the desired columns
+
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS contacts (
+                id integer primary key,
+                name VARCHAR(200),
+                mobile_no VARCHAR(10),
+                email VARCHAR(255) NULL)
+                ''')
+
+
+#### 3. Import CSV file into database
+
+## Specify the column indices you want to import (0-based index)
+## Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 1]
+
+# ## Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+
+# cursor.execute('''DELETE TABLE contacts''')
+
+## To insert contacts manually
+# query = "INSERT INTO contacts VALUES (null,'Gaurav Jadhav', '8698027152', null)"
+# cursor.execute(query)
+# con.commit()
+
+# query = 'aditya karosiya'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
--- engine/features.py
+++ engine/features.py
@@ -1,6 +1,9 @@
 
+from pipes import quote
 import re
+import subprocess
 from playsound import playsound
+import pyautogui
 from engine.command import *
 from engine.config import *
 import os
@@ -15,7 +18,7 @@ import struct
 import sqlite3
 import eel
 
-from engine.helper import extract_yt_term
+from engine.helper import extract_yt_term, remove_words
 
 
 # Playing assistant sound
@@ -136,3 +139,67 @@ def hotword():
         if paud is not None:
             paud.terminate()
     
+
+## find contacts number Function in features.py
+
+## Find contacts from database
+def findContact(query):
+    
+    
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'wahtsapp', 'video']
+    query = remove_words(query, words_to_remove)
+
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results = cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str = str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str = '+91' + mobile_number_str
+
+        return mobile_number_str, query
+    except:
+        speak('not exist in contacts')
+        return 0, 0
+    
+
+# Whatsapp Function 
+
+def whatsApp(mobile_no, message, flag, name):
+
+    if flag == 'message':
+        target_tab = 12
+        jarvis_message = "message send successfully to "+name
+
+    elif flag == 'call':
+        target_tab = 7
+        message = ''
+        jarvis_message = "calling to "+name
+
+    else:
+        target_tab = 6
+        message = ''
+        jarvis_message = "staring video call with "+name
+
+    # Encode the message for URL
+    encoded_message = quote(message)
+
+    # Construct the URL
+    whatsapp_url = f"whatsapp://send?phone={mobile_no}&text={encoded_message}"
+
+    # Construct the full command
+    full_command = f'start "" "{whatsapp_url}"'
+
+    # Open WhatsApp with the constructed URL using cmd.exe
+    subprocess.run(full_command, shell=True)
+    time.sleep(5)
+    subprocess.run(full_command, shell=True)
+    
+    pyautogui.hotkey('ctrl', 'f')
+
+    for i in range(1, target_tab):
+        pyautogui.hotkey('tab')
+
+    pyautogui.hotkey('enter')
+    speak(jarvis_message)
